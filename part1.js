98c27c2f5b691139020b4b913ac9ed2646ab0033
David <davca2322@gmail.com>
2021-08-04 21:27:18 -0400

    swiper

--- part1.js
+++ part1.js
@@ -16,13 +16,33 @@ const nobodyMan = {
 
 // Add your heroes here
 
+const rickRoll = {
+  name: "Rick Roll",
+  secretIdentity: "Rick Astley",
+  powers: ['Never give you up', 'Never let you down', 'Never turn around and desert you'],
+  team: 'The Trolls',
+  saying: "You know the rules and so do I",
+}
 
+const shaggy = {
+  name: "Ultra Instinct Shaggy",
+  secretIdentity: "Shaggy Rogers",
+  powers: ['Distort time and space', 'Eat scooby snacks'],
+  team: 'Does not need one',
+  saying: "Like, I'm not even using 1% of my power",
+}
 
+const beegBoy = {
+  name: "Beeg Yoshi",
+  secretIdentity: "Yoshi",
+  powers: ['his strong gaze', 'his beeg size', 'beeg, beeg, beeg'],
+  team: 'The big boys',
+  saying: "...",
+}
 
 // 2. Create an array called "heroes" and put your hero objects in it
 
-
-
+let heroes = [rickRoll, shaggy, beegBoy]
 
 /* 3. Create a loop that makes a <div> element for each hero in the array. The
  * div should contain the following:
@@ -49,7 +69,45 @@ const nobodyMan = {
  *
  * Finally, the loop should append the created div to the #hero-list element in
  * the index.html.
- */
+*/
+for(let i = 0; i < heroes.length; i++)
+{
+let div = document.createElement("div");
+let heroList = document.querySelector('#hero-list');
+let ht = document.createElement("h2");
+ht.innerHTML = heroes[i][`name`];
+let p = document.createElement("p");
+p.innerHTML = heroes[i][`secretIdentity`];
+let p2 = document.createElement("p");
+p2.innerHTML = heroes[i][`team`];
+p2.innerText = "Team Name: " + heroes[i].name;
+p.innerText = "Secret Identity: " + heroes[i].secretIdentity;
+
+
+
+let ul = document.createElement("ul");
+let h3 = document.createElement("h3");
+h3.innerHTML = "Powers:";
+ul.appendChild(h3)
+
+let list0 = document.createElement("li");
+list0.innerHTML = heroes[i].powers[0];
+let list1 = document.createElement("li");
+list1.innerHTML = heroes[i].powers[1];
+
+let saying = document.createElement("p");
+saying.innerHTML = heroes[i]["saying"];
+saying.innerText = `"` + heroes[i].saying + `"`;
+
+div.appendChild(ht);
+div.appendChild(p2);
+div.appendChild(p);
+div.appendChild(ul);
+div.appendChild(list0);
+div.appendChild(list1);
+div.appendChild(saying);
+heroList.appendChild(div);
+}
 
 // Part 1 complete!
 
--- part2.js
+++ part2.js
@@ -6,10 +6,100 @@
 // powers, team, and their saying. Give the form a button that says "Add Hero" and append it to the #form-box div
 
 
+let origin = document.querySelector('#form-box');
+let divhero = document.createElement("div");
+
+let labelhero = document.createElement("label");
+labelhero.innerHTML = "Name:";
+let heroinput = document.createElement("input");
+
+let labelhero2 = document.createElement("label");
+labelhero2.innerHTML = "Secret Identity:";
+let heroinput2 = document.createElement("input");
+
+
+let labelhero3 = document.createElement("label");
+labelhero3.innerHTML = "power:";
+let heroinput3 = document.createElement("input");
+
+
+let labelhero4 = document.createElement("label");
+labelhero4.innerHTML = "Superhero team:";
+let heroinput4 = document.createElement("input");
+
+
+let labelhero5 = document.createElement("label");
+labelhero5.innerHTML = "saying:";
+let heroinput5 = document.createElement("input");
+
+
+let Addhero = document.createElement("button");
+Addhero.innerHTML = "CLICK HERE TO ADD A HERO"
 
 // 2. Add an event listener to the form's button that checks for a click, then
 // calls a function called "addHero."
 
+
+
+divhero.appendChild(labelhero);
+divhero.appendChild(heroinput);
+divhero.appendChild(labelhero2);
+divhero.appendChild(heroinput2);
+divhero.appendChild(labelhero3);
+divhero.appendChild(heroinput3);
+divhero.appendChild(labelhero4);
+divhero.appendChild(heroinput4);
+divhero.appendChild(labelhero5);
+divhero.appendChild(heroinput5);
+
+
+Addhero.addEventListener('click',addhero);
+ function addhero(event) 
+ {
+     event.preventDefault();
+     let UHeroName = heroinput.value;
+     let UHerosecrett = heroinput2.value;
+     let UHeropower = heroinput3.value;
+     let UHeroteam = heroinput4.value;
+     let UHeroword = heroinput5.value;
+
+
+     let div = document.createElement("div");
+     let Acess = document.querySelector('#form-box');
+
+     let ht = document.createElement("h2");
+     ht.innerHTML = UHeroName;
+
+     let pt2 = document.createElement("p");
+     pt2.innerHTML = "Team Name: "+UHeroteam;
+
+     let pt = document.createElement("p");
+     pt.innerHTML = "Secret Identity: "+UHerosecrett;
+     
+     let ul = document.createElement("ul");
+     let h3 = document.createElement("h3");
+     h3.innerHTML = "Powers:";
+     ul.appendChild(h3)
+     
+     let list0 = document.createElement("li");
+     list0.innerHTML = UHeropower;
+     
+     let saying = document.createElement("p");
+     saying.innerHTML = "Hero Saying: "+ UHeroword;
+     
+     div.appendChild(ht);
+     div.appendChild(pt2);
+     div.appendChild(pt);
+
+     div.appendChild(ul);
+
+     div.appendChild(list0);
+     div.appendChild(saying);
+     Acess.appendChild(div);
+ }
+ 
+ origin.appendChild(divhero);
+ origin.appendChild(Addhero);
 
 
 /* 3. Modify the addHero function so that it creates an object using the values
